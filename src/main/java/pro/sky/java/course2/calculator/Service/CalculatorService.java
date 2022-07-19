e5db2946e8b2ea27d55af51f80117ae8dafcd63f
Trend <Trend778@gmail.com>
2022-07-19 23:01:47 +0400

    HW4 Commit

---
+++ src/main/java/pro/sky/java/course2/calculator/Service/CalculatorService.java
@@ -0,0 +1,24 @@
+package pro.sky.java.course2.calculator.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public double divide(int num1, int num2) {
+            return (double) num1 / num2;
+    }
+}
